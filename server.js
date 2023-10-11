49de663722bfdbe20345b5e07c90d43232270115
MWIZERWAFREDDY <mwizerwafreddy@gmail.com>
2023-10-11 19:21:45 +0200

    Your commit message

--- server.js
+++ server.js
@@ -13,15 +13,18 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/students", StudentRouter);
 
+const mongoURI = process.env.MONGODB_URI; // Access the environment variable
+
 mongoose
-  .connect(process.env.MONGODB_URL, {
+  .connect(mongoURI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("server is running and connected to db");
+      console.log("Server is running and connected to the database");
     });
   })
   .catch((error) => {
-    console.log("can't connect to db");
+    console.error("Error connecting to the database:", error);
   });
